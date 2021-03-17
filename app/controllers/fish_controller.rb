c906af212c70c2d29db85f6b210a665bcab02af8
Sahanon Phisetpakasit <sahanon.p@ku.th>
2021-03-17 20:48:52 +0700

    add CRUD

--- app/controllers/fish_controller.rb
+++ app/controllers/fish_controller.rb
@@ -1,8 +1,51 @@
 class FishController < ApplicationController
   def index
     @fish = Fish.all
+    @fish = @fish.page(params[:page]).per(5)
   end
+
+  def admin
+    @fish = Fish.all
+  end
+
   def show
     @fish = Fish.find(params[:id])
+  end
+
+  def new
+    puts "This is how you print out something."
+    @fish = Fish.new
+  end
+
+  def edit
+    @fish = Fish.find(params[:id])
+  end
+
+  def create
+    @fish = Fish.create(fish_params)
+
+    if @fish.invalid?
+      flash[:error] = @fish.errors.full_messages
+    end
+
+    redirect_to action: :index
+  end
+
+  def update
+    @fish = Fish.find(params[:id])
+    @fish.update(fish_params)
+    redirect_to action: :index
+  end
+
+  def destroy
+    @fish = Fish.find(params[:id])
+    @fish.destroy
+    redirect_to action: :index
+  end
+
+  private
+
+  def fish_params
+    params.require(:fish).permit(:title, :description,:stock,:price)
   end
 end
